b8d62b1a35bb214fa60f664843fa561b1e92197a
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-08-14 22:14:01 +0900

    Article Serviceの作成

--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,4 +1,30 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, Inject } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { Article } from './article.entity';
 
 @Injectable()
-export class ArticlesService {}
+export class ArticlesService {
+    constructor(
+        @InjectRepository(Article) private articleRepository: Repository<Article>,
+    ) { }
+
+    async getArticles(article: Article): Promise<Article[]> {
+        return await this.articleRepository.find();
+    }
+
+    async getArticle(_id: number): Promise<Article[]> {
+        return await this.articleRepository.find({
+            select: ['title', 'description', 'updateDate'],
+            where: [{ 'id': _id }],
+        });
+    }
+
+    async updateArticle(article: Article) {
+        this.articleRepository.save(article);
+    };
+
+    async deleteArticle(article: Article) {
+        this.articleRepository.delete(article);
+    }
+}
