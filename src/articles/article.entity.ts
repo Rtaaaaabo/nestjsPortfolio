c5d4a3fab969e729c37118a623a9799f9e70be55
Taku Nakagawa <takunakagawa@TakunoMac-mini.local>
2019-08-14 22:04:12 +0900

    ArticleのEntityを作成

--- src/articles/article.entity.ts
+++ src/articles/article.entity.ts
@@ -1,1 +1,30 @@
-export class Article.Entity {}
+import { Entity, Column, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity()
+export class Article {
+
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column({ length: 500 })
+    title: string;
+
+    @Column('text')
+    description: string;
+
+    @Column()
+    fileName: string;
+
+    @Column()
+    views: number;
+
+    @Column()
+    isPublished: boolean;
+
+    @Column('date')
+    createDate: Date;
+
+    @Column('date')
+    updateDate: Date;
+
+}
